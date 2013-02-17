94d3024f8cccd5ad9b9f287d0759df5536b50faa
Enel <spiritring@gmail.com>
2013-02-17 14:55:12 +0800

    V3
    V3

--- TSCocos2DX/TSLayer.cpp
+++ TSCocos2DX/TSLayer.cpp
@@ -40,24 +40,36 @@ bool TSLayer::init()
     // ask director the window size
     CCSize size = CCDirector::sharedDirector()->getWinSize();
     
-    // add "HelloWorld" splash screen"
+
     CCSprite* pSprite = CCSprite::create("background.png");
+    {
+        pSprite->setPosition( ccp(size.width/2, size.height/2) );
+        pSprite->setScale(2);
+        this->addChild(pSprite, 0, 1000);
+    }
+
+    CCSprite* pMesh = CCSprite::create("board_test.png");
+    {
+        pMesh->setPosition(ccp(size.width/2, size.height/2));
+        this->addChild(pMesh, 0, 1001);
+    }
     
-    // position the sprite on the center of the screen
-    pSprite->setPosition( ccp(size.width/2, size.height/2) );
+    CCRect pR = pMesh->getTextureRect();
+    CCPoint pOO = ccp(size.width/2 - pR.size.width/2, size.height/2 - pR.size.height/2);
     
-    // add the sprite as a child to this layer
-    this->addChild(pSprite, 0);
+    CCSprite* pBall = CCSprite::create("chess0.png");
+    pBall->setPosition(pOO);
+    this->addChild(pBall, 2, 1);
     
-    // add a label shows "Hello World"
-    // create and initialize a label
-    CCLabelTTF* pLabel = CCLabelTTF::create("Hello World", "Thonburi", 34);
-    
-    // position the label on the center of the screen
-    pLabel->setPosition( ccp(size.width / 2, size.height - 20) );
-    
-    // add the label as a child to this layer
-    this->addChild(pLabel, 1);
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            CCSprite* pT = CCSprite::create("chess1.png");
+            CCPoint pX = ccp(pOO.x + i * 33 + 32/2, pOO.y + j * 33 + 32/2);
+            pT->setPosition(pX);
+            pT->setScale(2);
+            this->addChild(pT, 2, 1);
+        }
+    }
     
     return true;
 }
